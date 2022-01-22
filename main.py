0b094a891639232f5dac877a671ef4f7945ea7e5
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-22 18:50:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Create a class Person with atributes Name, age, salalry and a method display() for showing the details. Create two instances of the class and call the method for each instance 
+class Person:
+  def _init_(self,name,age,salary):
+    self.name=name
+    self.age=age
+    self.salary=salary
+
+  def display(self):
+    print("Person Details:-")
+    print("Name:",self.name)
+    print("Age:",self.age)
+    print("Salary:",self.salary)
+    
+
+p1=Person("Micheal",24,254000)
+p1.display()
+
+p2=Person("Samuel",30,558000)
+print("\n")
+p2.display()
